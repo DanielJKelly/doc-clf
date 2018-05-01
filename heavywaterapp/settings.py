7d98275818a3bab4cb317124f8e7b2ccc542647b
Daniel Kelly <djk8552@gmail.com>
2018-05-01 17:05:28 -0400

    Update settings.py

--- heavywaterapp/settings.py
+++ heavywaterapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from . import settings_secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SECRET_KEY = settings_secret.SECRET_KEY
+SECRET_KEY = os.environ.get(SECRET_KEY)
 
 ALLOWED_HOSTS = [
     'django-env3.p3r3dh3xti.us-east-1.elasticbeanstalk.com',
